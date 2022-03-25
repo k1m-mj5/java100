2ddf8ce21ce4d02d6691aace170206658907e0c5
k1m-mj5 <77222844+k1m-mj5@users.noreply.github.com>
2022-03-25 20:51:15 +0900

    data type

---
+++ Java100_variable_DataType2.java
@@ -0,0 +1,23 @@
+public class Java100_variable_DataType2 {
+	public static void main(String[] args) {
+		
+		byte b = 127;
+		short s = 32767;
+		int i = 2147483647;
+		long l = 7000000000L;
+		float f = 9.8F;
+		double d = 3.14;
+		char c = 'A';
+		boolean bl = false;
+		
+		System.out.println(b+"byte MAX-->"+Byte.MAX_VALUE);
+		System.out.println(s+"short MAX-->"+Short.MAX_VALUE);
+		System.out.println(i+"int MAX-->"+Integer.MAX_VALUE);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(bl);
+		
+	}
+}
---
+++ Java100_variable_DataType3.java
@@ -0,0 +1,13 @@
+public class Java100_variable_DataType3 {
+	public static void main(String[] args) {
+		
+		short a = 'A';
+		System.out.println(a);  // A(65)
+		
+		short b = 90;
+		System.out.println((char)b);  // A(65)+(26-1) = 90 = Z
+		
+		char c = 'a';
+		System.out.println((short)c); // a(97) 
+	}
+}
