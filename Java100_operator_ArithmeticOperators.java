ab6d9ecb8825c615b695fbd3d8b283cbe7b4d2d6
k1m-mj5 <77222844+k1m-mj5@users.noreply.github.com>
2022-03-28 17:31:52 +0900

    operators

---
+++ Java100_operator_ArithmeticOperators.java
@@ -0,0 +1,14 @@
+public class Java100_operator_ArithmeticOperators{
+	public static void main(String[] args) {
+		
+		int a,b,c,d;
+		a=60; b=8; c=300; d=400;
+		
+		System.out.println(a+b); 				// 68
+		System.out.println("a+b="+(a+b)); // a+b=68
+		System.out.println("a-b="+(a-b));	// a-b=52
+		System.out.println("a*b="+(a*b));	// a*b=480
+		System.out.println("a/b="+(a/b));	// a/b=7
+		System.out.println("a%b="+(a%b)); // a%b=4
+	}
+}
---
+++ Java100_operator_ArithmeticOperators3.java
@@ -0,0 +1,24 @@
+public class Java100_operator_ArithmeticOperators3 {
+	public static void main(String[] args) {
+		
+		int a=0, b=100;
+		
+		a=a+1;
+		System.out.println(a); 	// 1
+		
+		a+=1;
+		System.out.println(a);	// 2
+		
+		a-=1;
+		System.out.println(a);	// 1
+		
+		b++;
+		System.out.println(b);	// 101
+		
+		b++;
+		System.out.println(b);	// 102
+		
+		b--;
+		System.out.println(b);	// 101
+	}
+}
