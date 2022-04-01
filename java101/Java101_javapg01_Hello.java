509d79533b300adeba41e52caebe560c92188fd8
k1m-mj5 <77222844+k1m-mj5@users.noreply.github.com>
2022-04-01 16:07:13 +0900

    Add files via upload

---
+++ java101/Java101_javapg01_Hello.java
@@ -0,0 +1,11 @@
+/*
+The Hello class implements and application that
+simply prints "Hello, Java Application!" to standard output.
+*/
+
+public class Java101_javapg01_Hello {
+	public static void main(String args[]) { // main method
+		// Display the string
+		System.out.println("Hello, Java Application!");
+	}
+}
